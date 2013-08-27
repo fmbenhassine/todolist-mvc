87c66973690702ca6b7bccec43c1ced4893c3492
natlantisprog <natlantisprog@gmail.com>
2013-08-27 21:54:58 +0200

    Suppression d'un import qui ne servait à rien

--- todolist-web-tapestry/src/main/java/net/benas/todolist/web/tapestry/pages/user/account/Update.java
+++ todolist-web-tapestry/src/main/java/net/benas/todolist/web/tapestry/pages/user/account/Update.java
@@ -29,7 +29,6 @@ import net.benas.todolist.core.service.api.UserService;
 import net.benas.todolist.web.tapestry.pages.user.Home;
 import org.apache.tapestry5.EventConstants;
 import org.apache.tapestry5.annotations.OnEvent;
-import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.SessionState;
 import org.apache.tapestry5.ioc.annotations.Inject;
