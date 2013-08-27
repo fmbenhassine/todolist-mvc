b1eeb9e7ee3db5a7dada69389b306556ad6f84e2
natlantisprog <natlantisprog@gmail.com>
2013-08-27 21:50:42 +0200

    Correction de l'update des données du profil.
    La façon de travailler avec les @Property n'est pas la bonne, puisque l'on a en value, côté .tml, un lien direct avec le loggedUser.
    
    Ex: t:value="prop:loggedUser.lastname"
    
    Si l'on veut passer pour la phase d'update sur un champ textuel séparé, il faut alors utiliser :
    
    Ex: t:value="lastName"
    
    Et ainsi ce sera le property private String lastName qui serait utilisé.

--- todolist-web-tapestry/src/main/java/net/benas/todolist/web/tapestry/pages/user/account/Update.java
+++ todolist-web-tapestry/src/main/java/net/benas/todolist/web/tapestry/pages/user/account/Update.java
@@ -29,6 +29,7 @@ import net.benas.todolist.core.service.api.UserService;
 import net.benas.todolist.web.tapestry.pages.user.Home;
 import org.apache.tapestry5.EventConstants;
 import org.apache.tapestry5.annotations.OnEvent;
+import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.SessionState;
 import org.apache.tapestry5.ioc.annotations.Inject;
@@ -46,20 +47,8 @@ public class Update {
     @SessionState
     private User loggedUser;
 
-    @Property
-    private String firstName;
-
-    @Property
-    private String lastName;
-
-    @Property
-    private String email;
-
     @OnEvent(value = EventConstants.SUCCESS, component = "updateAccountForm")
     public Object onSuccess() {
-        loggedUser.setFirstname(firstName);
-        loggedUser.setLastname(lastName);
-        loggedUser.setEmail(email);
         userService.update(loggedUser);
         return Home.class;
     }
